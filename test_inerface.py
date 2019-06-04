43a29460f3db0c28768cde58ed4dcd8c7ad3fc74
fosterwu1978 <2055739@qq.com>
2019-06-04 11:56:15 +0800

    add new blank line!

--- test_inerface.py
+++ test_inerface.py
@@ -19,4 +19,6 @@ data = json.dumps({'username':'吴老师'})
 r = requests.post('http://localhost:8080/IDEAmaven/springboot/getUserByJson',data)
 print(r.text)
 
+
+
 assert r.text == '{"userdata":"吴老师 is a man!"}'
